e6929ddec5fcd2c4aa21446c41527d7851c89abb
nokks <nokkynd@yahoo.com>
2014-07-12 13:35:22 +0100

    integrated wolfram

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import bottle
+from bottle import request
+import wolframalpha
 
+client = wolframalpha.Client("5UL6XH-64G6L9Y5R4")
 app = bottle.Bottle()
+
+bottle.debug = True
 
 @app.get("/")
 def home():
-  return "Hello World"
-
+	query = request.query["q"]
+	terms = query.split(":")
+	strq = "".join(terms[1:])
+	print("QUERY: %s"%strq)
+	
+	res = client.query(strq)
+	try:
+		return next(res.results).text
+	except:
+		return ""
+	
 def main():
   app.run(host="0.0.0.0", port=8080)
 
